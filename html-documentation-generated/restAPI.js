8ae7f8afb8641bd8025355c9703ed82c87b5f976
Tapani <t8sima01@students.oamk.fi>
2020-10-13 13:10:58 +0300

    Tasks added
    OPENAPI document, HTML, document & REST API implementation.

---
+++ html-documentation-generated/restAPI.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3400;
+
+app.use(express.static('views'))
+app.get('', (req, res) => {
+    res.render(__dirname +'/views/index.html')
+});
+app.listen(port);
